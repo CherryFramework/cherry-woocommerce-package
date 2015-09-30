4235d21236bfc9494210d97838222fd4c9a34f29
mjhead <andremjhead@gmail.com>
2015-09-30 17:20:58 +0300

    FIX: PHP minor code style fixes to pass Travis build

--- public/includes/class-cherry-wc-options.php
+++ public/includes/class-cherry-wc-options.php
@@ -143,8 +143,8 @@ if ( ! class_exists( 'Cherry_WC_Options' ) ) {
 		 *
 		 * @since 1.0.0
 		 * @uses cherry_get_option()
-		 * @param string  $option option name.
-		 * @param mixed   $default default option value.
+		 * @param string $option option name.
+		 * @param mixed  $default default option value.
 		 * @return mixed
 		 */
 		public function get_option( $option, $default = false ) {
