4b1cdcb589a18fa8975f6e15a709854edaf22b83
mjhead <andremjhead@gmail.com>
2015-11-10 12:29:36 +0200

    UPD: revert version to 1.0.0

--- cherry-woocommerce-package.php
+++ cherry-woocommerce-package.php
@@ -3,7 +3,7 @@
  * Plugin Name: Cherry WooCommerce package
  * Plugin URI:  http://www.cherryframework.com/
  * Description: Extends Cherryframework for WooCommerce
- * Version:     1.0.1
+ * Version:     1.0.0
  * Author:      Cherry Team
  * Author URI:  http://www.cherryframework.com/
  * Text Domain: cherry-woocommerce-package
@@ -67,7 +67,7 @@ if ( ! class_exists( 'Cherry_Woocommerce_Package' ) ) {
 		 * @since 1.0.0
 		 * @var   string
 		 */
-		public $version = '1.0.1';
+		public $version = '1.0.0';
 
 		/**
 		 * Class constructor
